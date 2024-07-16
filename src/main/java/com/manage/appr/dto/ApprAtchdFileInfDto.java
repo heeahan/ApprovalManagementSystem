45816e1483254185972311789d17afda7061beb8
Administrator <ahee0826@gmail.com>
2024-07-16 10:36:35 +0900

    Initial commit

--- src/main/java/com/manage/appr/dto/ApprAtchdFileInfDto.java
+++ src/main/java/com/manage/appr/dto/ApprAtchdFileInfDto.java
@@ -1,4 +1,25 @@
 package com.manage.appr.dto;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ApprAtchdFileInfDto {
+    private Long apprAtchdFileId;
+    private Long apprAtchdFileSrno;
+    private Long fileId;
+    private String fileNm;
+    private String filePath;
+    private Long apprId;
+    private String frstRegUserId;
+    private LocalDateTime frstRegDtmt;
+    private String lastChgUserId;
+    private LocalDateTime lastChgDtmt;
 }
